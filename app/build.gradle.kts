0a87811505bec77cf3f9ce5a9c906e8335df687e
Ventaxis <egorklofc2000@gmail.com>
2026-07-21 16:58:04 +0400

    Update build.gradle.kts
    Updated plugin versions for Android and Kotlin.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,14 +1,6 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("com.android.application") version "8.2.0"
+    id("org.jetbrains.kotlin.android") version "1.9.22"
 }
 
 android {
